b8bc6a1905336cad64276ee3763ecfb7c63a015d
vmiroshnichenko <vyacheslav.miroshnichenko@netcracker.com>
2017-03-03 12:42:56 +0200

    update Binary Tree

--- src/main/java/slavik/sendbox/pr_algorithms_SSU/pr4/binary_tree/BinarySearchTree.java
+++ src/main/java/slavik/sendbox/pr_algorithms_SSU/pr4/binary_tree/BinarySearchTree.java
@@ -3,37 +3,93 @@ package slavik.sendbox.pr_algorithms_SSU.pr4.binary_tree;
 /**
  * Created by vymi1016 on 03/02/2017.
  */
-public class BinarySearchTree<T extends Comparable> {
-    public Node<T> find(Node<T> root, T x)
-    {
-        if(root==null)
-            return null;
-        if(root.key==x)
-            return root;
-        if(root.key.compareTo(x)==1){
+public class BinarySearchTree{
+    private Node root;
 
-            return find(root.r, x);
-        }
-        else{
-            return find(root.l, x);
-        }
+    public  BinarySearchTree() {
+        this.root = null;
     }
-    public Node<T> insert(Node<T> root, T x)
-    {
-        if(root==null) {
-            Node temp =  new Node<T>();
-            temp.key = x;
-            return temp;
-        }
+    public void insert(int data) {
+        root = insert(root, data, null);
+    }
 
-        if(root.key.compareTo(x)==1){
-
-            root.r = insert(root.r, x);
+    private Node insert(Node current, int data, Node parent) {
+        if (current == null) {
+            current = new Node();
+            current.setData(data);
+            current.setLeft(null);
+            current.setRight(null);
+            current.setParent(parent);
+        } else if (data < current.getData()) {
+            current.setLeft(insert(current.getLeft(), data, current));
+        } else {
+            current.setRight(insert(current.getRight(), data, current));
         }
-        if(root.key.compareTo(x)==-1){
+        return current;
+    }
 
-            root.l = insert(root.l, x);
+
+    public void delete(int data) {
+        root = delete(root, data);
+    }
+
+    private Node delete(Node startNode, int data) {
+        Node element = find(startNode, data);
+        if (element == null) return startNode;
+        boolean hasParent = element.getParent() != null;
+        boolean isLeft = hasParent && element.getData() < element.getParent().getData();
+        if (element.getLeft() == null && element.getRight() == null) {
+            if (hasParent) {
+                if (isLeft) {
+                    element.getParent().setLeft(null);
+                } else {
+                    element.getParent().setRight(null);
+                }
+            }
+        } else if (element.getLeft() != null && element.getRight() == null) {
+            if (hasParent) {
+                if (isLeft) {
+                    element.getParent().setLeft(element.getLeft());
+                } else {
+                    element.getParent().setRight(element.getLeft());
+                }
+            } else {
+                startNode = element.getLeft();
+            }
+        } else if (element.getLeft() == null && element.getRight() != null) {
+            if (hasParent) {
+                if (isLeft) {
+                    element.getParent().setLeft(element.getRight());
+                } else {
+                    element.getParent().setRight(element.getRight());
+                }
+            } else {
+                startNode = element.getRight();
+            }
+        } else {
+            Node rightMin = findMin(element.getRight());
+            element.setData(rightMin.getData());
+            return delete(rightMin, rightMin.getData());
         }
-        return root;
+        element = null;
+        return startNode;
+    }
+    public Node findMin(Node min) {
+        if (min == null) return null;
+        while (min.getLeft() != null) {
+            min = min.getLeft();
+        }
+        return min;
+    }
+    public Node find(int data) {
+        return find(root, data);
+    }
+
+    private Node find(Node current, int data) {
+        if (current == null)
+            return null;
+        if (current.getData() == data)
+            return current;
+        return find(data < current.getData() ? current.getLeft() : current.getRight(), data);
     }
 }
--- src/main/java/slavik/sendbox/pr_algorithms_SSU/pr4/binary_tree/Node.java
+++ src/main/java/slavik/sendbox/pr_algorithms_SSU/pr4/binary_tree/Node.java
@@ -3,7 +3,41 @@ package slavik.sendbox.pr_algorithms_SSU.pr4.binary_tree;
 /**
  * Created by vymi1016 on 03/02/2017.
  */
-public class Node<T extends Comparable> {
-    T key;
-    Node l,r;
+public class Node {
+    private int data;
+    private Node left;
+    private Node right;
+    private Node parent;
+
+    public int getData() {
+        return this.data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public Node getLeft() {
+        return this.left;
+    }
+
+    public void setLeft(Node left) {
+        this.left = left;
+    }
+
+    public Node getRight() {
+        return this.right;
+    }
+
+    public void setRight(Node right) {
+        this.right = right;
+    }
+
+    public Node getParent() {
+        return this.parent;
+    }
+
+    public void setParent(Node parent) {
+        this.parent = parent;
+    }
 }
